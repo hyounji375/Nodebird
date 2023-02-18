fd6a1c51c4581dd784c7881de3862aff42cac731
hyounji375 <hyounji375@gmail.com>
2023-02-19 01:15:19 +0900

    반응형 그리드 추가

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,6 +1,6 @@
 import propTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
@@ -17,12 +17,31 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        {/* gutter : 컬럼 사이에 간격을 주는 것 */}
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://github.com/hyounji375"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {/* target="_blank"로 새 창에서 띄우면 보안 위협이 있어서 보통 rel="noreferrer noopener"을 적어줌. */}
+            Made by Editha
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
