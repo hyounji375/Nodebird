e0e11667686125cb9b3a6ecc2e02e9e619e47f96
hyounji375 <hyounji375@naver.com>
2023-02-19 16:52:01 +0900

    회원가입 폼 만들기
    useinput

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -2,6 +2,8 @@ import { Form, Input, Button } from "antd";
 import { useCallback, useState } from "react";
 import Link from "next/link";
 import styled from "styled-components";
+import PropTypes from "prop-types";
+import useinput from "../hooks/useinput";
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -12,16 +14,8 @@ const FormWrapper = styled(Form)`
 `;
 
 const LoginForm = ({ setIsLoggedIn }) => {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onChangeId = useCallback((e) => {
-    setId(e.target.value);
-  }, []);
-
-  const onChangePassword = useCallback((e) => {
-    setPassword(e.target.value);
-  }, []);
+  const [id, onChangeId] = useinput("");
+  const [password, onChangePassword] = useinput("");
 
   const onSubmitForm = useCallback(() => {
     console.log(id, password);
@@ -59,4 +53,9 @@ const LoginForm = ({ setIsLoggedIn }) => {
     </FormWrapper>
   );
 };
+
+LoginForm.propTypes = {
+  setIsLoggedIn: PropTypes.func.isRequired,
+};
+
 export default LoginForm;
